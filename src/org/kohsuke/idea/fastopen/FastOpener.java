1f13dc60d1a812255038551506234c12c3d09cde
Serge Baranov <sbaranov@gmail.com>
2012-04-25 18:54:28 +0400

    IDEA 11 adapted version with .idea directory based project format support and fixed menu item position

--- src/org/kohsuke/idea/fastopen/FastOpener.java
+++ src/org/kohsuke/idea/fastopen/FastOpener.java
@@ -10,136 +10,133 @@ import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.UnsupportedFlavorException;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.List;
 
 /**
  * @author Kohsuke Kawaguchi
+ * @author Serge Baranov
  */
 public class FastOpener extends AnAction {
-    private File getProjectFromClipboard() {
-        File f = getFileFromClipboard();
-        if(f==null) return null;
+  private File getProjectFromClipboard() {
+    File f = getFileFromClipboard();
+    if (f == null) return null;
 
-        return adjust(f);
+    return adjust(f);
+  }
+
+  public static File adjust(File f) {
+    String n = f.getName();
+    if (n.endsWith(".ipr") || n.equals("pom.xml"))
+      return f;   // project file name is selected
+
+    if (f.isDirectory()) {
+      // directory is selected. Find the project file or use .idea project subdirectory
+      File[] children = f.listFiles();
+      if (children != null) {
+        for (File child : children) {
+          if (child.getName().endsWith(".ipr"))
+            return child;
+          else if (child.isDirectory() && child.getName().equalsIgnoreCase(".idea"))
+            return f;
+        }
+        // next attempt is to find POM
+        for (File child : children) {
+          if (child.getName().equals("pom.xml"))
+            return child;
+        }
+      }
     }
 
-    public static File adjust(File f) {
-        String n = f.getName();
-        if(n.endsWith(".ipr") || n.equals("pom.xml"))
-            return f;   // project file name is selected
+    // not a project file
+    return null;
+  }
 
-        if(f.isDirectory()) {
-            // directory is selected. Find the project file
-            File[] children = f.listFiles();
-            for( File child : children ) {
-                if(child.getName().endsWith(".ipr"))
-                    return child;
-            }
-            // next attempt is to find POM
-            for( File child : children ) {
-                if(child.getName().equals("pom.xml"))
-                    return child;
-            }
-        }
+  private File getFileFromClipboard() {
+    Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 
-        // not a project file
-        return null;
+    try {
+      if (clipboard.isDataFlavorAvailable(DataFlavor.stringFlavor)) {
+        String data = clipboard.getData(DataFlavor.stringFlavor).toString();
+        File f = new File(data.trim());
+        if (f.exists()) return f;
+      }
+
+      if (clipboard.isDataFlavorAvailable(DataFlavor.javaFileListFlavor)) {
+        @SuppressWarnings("unchecked")
+        List<File> data = (List<File>) clipboard.getData(DataFlavor.javaFileListFlavor);
+        if (!data.isEmpty()) return data.get(0);
+      }
+    } catch (UnsupportedFlavorException ignored) {
+    } catch (IOException ignored) {
     }
 
-    private File getFileFromClipboard() {
-        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-
-        try {
-            if(clipboard.isDataFlavorAvailable(DataFlavor.stringFlavor)) {
-                String data = clipboard.getData(DataFlavor.stringFlavor).toString();
-                File f =  new File(data.trim());
-                if(f.exists())  return f;
-            }
-
-            if(clipboard.isDataFlavorAvailable(DataFlavor.javaFileListFlavor)) {
-                List<File> data = (List<File>) clipboard.getData(DataFlavor.javaFileListFlavor);
-                if(!data.isEmpty()) return data.get(0);
-            }
-        } catch (UnsupportedFlavorException e) {
-            // fall through
-        } catch (IOException e) {
-            // fall through
-        }
-
-        // if this is Unix, try xclip
-        // one would expect that this is unnecessary, but somehow the above scheme
-        // doesn't catch the contents xclip put into the clipboard.
-        if(File.separatorChar=='/') {
+    // if this is Unix, try xclip
+    // one would expect that this is unnecessary, but somehow the above scheme
+    // doesn't catch the contents xclip put into the clipboard.
+    if (File.separatorChar == '/') {
+      try {
+        final File[] result = new File[1];
+        Thread t = new Thread() {
+          public void run() {
+            Process p = null;
             try {
-                final File[] result = new File[1];
-                Thread t = new Thread() {
-                    public void run() {
-                        Process p = null;
-                        try {
-                            ByteArrayOutputStream buf = new ByteArrayOutputStream();
-                            ProcessBuilder b = new ProcessBuilder("xclip", "-o", "-selection", "clipboard");
-                            b.redirectErrorStream(true);
-                            p = b.start();
-                            p.getOutputStream().close();
-                            copyStream(p.getInputStream(), buf);
-                            p.waitFor();
-                            result[0] = new File(new String(buf.toByteArray()).trim());
-                        } catch (IOException e) {
-                            // ignore
-                            if(p!=null) p.destroy();
-                        } catch (InterruptedException e) {
-                            // ignore
-                            if(p!=null) p.destroy();
-                        }
-                    }
-                };
-                t.start();
-                t.join(1000);
-                t.interrupt(); // abandon the processing if it's taking too long
-                if(result[0]!=null && result[0].exists())  return result[0];
+              ByteArrayOutputStream buf = new ByteArrayOutputStream();
+              ProcessBuilder b = new ProcessBuilder("xclip", "-o", "-selection", "clipboard");
+              b.redirectErrorStream(true);
+              p = b.start();
+              p.getOutputStream().close();
+              copyStream(p.getInputStream(), buf);
+              p.waitFor();
+              result[0] = new File(new String(buf.toByteArray()).trim());
+            } catch (IOException e) {
+              if (p != null) p.destroy();
             } catch (InterruptedException e) {
-                // fall through
+              p.destroy();
             }
-        }
-
-        return null;
+          }
+        };
+        t.start();
+        t.join(1000);
+        t.interrupt(); // abandon the processing if it's taking too long
+        if (result[0] != null && result[0].exists()) return result[0];
+      } catch (InterruptedException ignored) {
+      }
     }
 
-    private void copyStream(InputStream i, OutputStream o) throws IOException {
-        byte[] buf = new byte[1024];
-        int len;
+    return null;
+  }
 
-        while((len=i.read(buf))>=0)
-            o.write(buf,0,len);
-        i.close();
-        o.close();
+  private void copyStream(InputStream i, OutputStream o) throws IOException {
+    byte[] buf = new byte[1024];
+    int len;
+
+    while ((len = i.read(buf)) >= 0)
+      o.write(buf, 0, len);
+    i.close();
+    o.close();
+  }
+
+  public void update(AnActionEvent event) {
+    Presentation p = event.getPresentation();
+
+    File f = getProjectFromClipboard();
+    if (f != null) {
+      p.setEnabled(true);
+      p.setText("Open Project _Fast [.../" + f.getParentFile().getName() + '/' + f.getName() + "]");
+    } else {
+      p.setEnabled(false);
+      p.setText("Open Project _Fast");
     }
+  }
 
-    public void update(AnActionEvent event) {
-        Presentation p = event.getPresentation();
-
-        File f = getProjectFromClipboard();
-        if(f!=null) {
-            p.setEnabled(true);
-            p.setText("Open Project _Fast [.../"+f.getParentFile().getName()+'/'+f.getName()+"]");
-        } else {
-            p.setEnabled(false);
-            p.setText("Open Project _Fast");
-        }
+  public void actionPerformed(AnActionEvent e) {
+    try {
+      File f = getProjectFromClipboard();
+      if (f != null)
+        ProjectUtil.openOrImport(f.getAbsolutePath(), null, true);
+    } catch (Exception x) {
+      Messages.showErrorDialog(x.getMessage(), "Error!");
     }
-
-    public void actionPerformed(AnActionEvent e) {
-        try {
-            File f = getProjectFromClipboard();
-            if(f!=null)
-                 ProjectUtil.openOrImport(f.getAbsolutePath(), null, true);
-        } catch (Exception x) {
-            Messages.showErrorDialog(x.getMessage(),"Error!");
-        }
-    }
+  }
 }
